66ea78043b07b58fb883b1ce49d1178118481b1c
kai.lv <kai@163.com>
2021-11-04 11:23:53 +0800

    third commit

--- src/main/java/com/kai/GitTest.java
+++ src/main/java/com/kai/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("git test2");
+        System.out.println("git test3");
     }
 
 
